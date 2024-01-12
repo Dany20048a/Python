1ab444b583995b3814db1c676383458731528bb6
Dany Moran <daniela.moranOC@gmail.com>
2024-01-12 17:10:59 -0600

    Feat: :sparkles: Create endpoints for Room and Users

--- backend/core_backend/urls.py
+++ backend/core_backend/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import functions_users
+from .views import functions_usuario
 
 users_url = [
-   path('hello_world',functions_users.hello_world,name='hello_world'), #name=hello (endpoints)
+   path('create_users',functions_usuario.create_users,name='create_users'), #name=hello (endpoints)
+   path('get_users',functions_usuario.get_users,name='get_users'),
+   path('get_usersid',functions_usuario.get_usersid,name='get_usersid'),
+   path('update_users',functions_usuario.update_users,name='update_users'),
+   path('delete_users',functions_usuario.delete_users,name='delete_users')
    #Nombre de la aplicación
 ]
 
